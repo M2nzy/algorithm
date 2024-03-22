7269dc87d4206e78cebfedfa60191c369ed33efe
M2nzy <m1nzz@naver.com>
2024-03-22 17:55:55 +0900

    240322

--- greedy/bj_11399.py
+++ greedy/bj_11399.py
@@ -1,8 +1,18 @@
 #입력받은 시간 정렬 후
 #앞시간 + 현재시간
 import sys
-n =  int(input())
-time = [int(sys.stdin.readline().strip()) for i in range(n)]
-
+n = int(input())
+time = list(map(int,sys.stdin.readline().split()))
+result = 0
 time.sort()
-print(time)
+
+def selfsum(a):
+    if a <= 1:
+        return a
+    return a + selfsum(a-1)
+
+for i in range(n):
+    result += selfsum(time[i])
+    print(result)
+
+print(result)

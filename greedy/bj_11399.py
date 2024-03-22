4b0b8a3880f58ad11db778513c9130246b220182
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-03-23 02:47:37 +0900

    240323

--- greedy/bj_11399.py
+++ greedy/bj_11399.py
@@ -1,18 +1,23 @@
 #입력받은 시간 정렬 후
-#앞시간 + 현재시간
+#현재 리스트 요소까지 전부 더하기
 import sys
-n = int(input())
+n = int(sys.stdin.readline())
 time = list(map(int,sys.stdin.readline().split()))
 result = 0
 time.sort()
-
-def selfsum(a):
-    if a <= 1:
-        return a
-    return a + selfsum(a-1)
+print("time:",time)
 
 for i in range(n):
-    result += selfsum(time[i])
-    print(result)
+    # While문
+    # j = 0
+    # while(True):
+    #     if(j==i+1):
+    #         break
+    #     else:
+    #         result += time[j]
+    #         j += 1
+        
+    for j in range(i+1):
+        result += time[j]
 
 print(result)

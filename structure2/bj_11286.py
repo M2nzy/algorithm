a8017c56529a4507a4c069fefb764adec4eabad4
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-05-24 02:13:42 +0900

    240523

--- structure2/bj_11286.py
+++ structure2/bj_11286.py
@@ -2,18 +2,17 @@ import heapq
 import sys
 N = int(sys.stdin.readline().strip())
 arr = []
-absarr = []
-arr2 = []
+
 for i in range(N):
     x = int(sys.stdin.readline().strip())
     if x != 0:
-        heapq.heappush(arr, x)
-        heapq.heappush(arr2, abs(x))
+        heapq.heappush(arr, (abs(x), x))
     else:
         if not arr:
             print(0)
         else:
-            if abs(arr[0]) in arr:
-                heapq.heappop(arr)
-        
-print(printarr)
+            print(heapq.heappop(arr)[1])
+
+
+    
+                        

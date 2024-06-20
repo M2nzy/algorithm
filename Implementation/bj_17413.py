a69b83ffe2515d66afd248be3c858981f45e58c0
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-06-20 19:53:34 +0900

    240620

--- Implementation/bj_17413.py
+++ Implementation/bj_17413.py
@@ -1,71 +1,53 @@
 import sys
-import copy
-S = sys.stdin.readline().strip()
-St = copy.deepcopy(S)
+input= sys.stdin.readline
 
-answer = []
-arr = []
-if '<' in St:
+S = input().strip()
 
-    indexLeft = St.find('<')
-    indexRight = St.find('>')
-    arr.append(St[:indexRight+1])
-    St = St[indexRight+1:]
-    while(True):     
-        indexLeft = St.find('<')
-        indexRight = St.find('>')
-        if indexRight == -1:
-            arr.append(St[0:])
-            break
-        else:
-            arr.append(St[:indexLeft])
-            arr.append(St[indexLeft:indexRight+1])
-            St = St[indexRight+1:]
+print(S)
+tagCheck = 0
+i = 0
+N = len(S)
+result = ""
+tagClose = 0
+first = 0
+tmp = ""
+for i in range(len(S)):
+
+    if S[i] == '<':
+        tagStart = i
+        if tmp:
+            result += "".join(tmp[::-1])  
+            tmp = "" 
+        tagCheck = 1
+        
+
+    elif S[i] == '>':
+        tagCheck = 0 
+        tagClose = i
+        tagLen = tagClose - tagStart
+        result += "".join(S[tagStart:tagClose+1])
+        first = i
+
+    elif S[i] == ' ':
+        if tagCheck == 1:
+            continue
+        elif tagCheck == 0: # just blank
+            print(i, first)
+            print("tmp:",tmp)
+            result += "".join(tmp[i:(first+1):-1])    
+            result += " "
+            first = i
     
+    elif tagCheck == 0:
+        tmp += S[i]
+        print(tmp)
 
-else:
-    arr = St.split()
+
+    if (i == (len(S)-1)) and (i != first):        
+
+        result += "".join(S[i:first-1:-1])
 
 
-for i in arr:
+        
 
-    if '<' in i:
-#        print(i, end='')    
-        answer.append(i)
-    else:
-        if (' ' in i) and ('<' not in i):
-            
-            a = i.split(' ')
-            for j in range(len(a)):
-                for k in range(1, (len(a[j])+1)):
-  #                      print(a[j][-k], end='')
-                        answer.append(a[j][-k])
-                if j+1 == len(a):
- #                   print('', end='')
-                    answer.append('')
-                else:
-#                    print(' ', end='')
-                    answer.append(' ')
-        else:
-            for j in range(1, (len(i)+1)):
-#                print(i[-j],end='')
-                answer.append(i[-j])
-    if '<' in S:
-#        print('',end='')
-        answer.append('')
-    else:
-#        print(' ',end='')
-        answer.append(' ')
-
-answer.reverse()
-for i in range(len(answer)):
-    if answer[0] == '':
-        answer.pop(0)
-    elif answer[0] == ' ':
-        answer.pop(0)
-    else:
-        break
-answer.reverse()
-
-for i in range(len(answer)):
-    print(answer[i], end='')
+print(result)

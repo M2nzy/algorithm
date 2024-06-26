bf39e43e89dbaee0b064381804c0823515f83052
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-06-26 17:45:33 +0900

    240626: tree

--- tree/bj_9934.py
+++ tree/bj_9934.py
@@ -4,8 +4,16 @@ input = sys.stdin.readline
 K = int(input().strip())
 
 
+tree = {}
 order = list(map(int, input().split()))
 
-tree = {}
+order.insert(0, False)
+print(order)
+for i in range(1, int((len(order)+1)/2)):
+    tree[order[i+1]] = [order[i], order[i+2]]
 
 
+
+print(tree)
+
+# DFS 돌렸을때 출력되는 순서를 반대로

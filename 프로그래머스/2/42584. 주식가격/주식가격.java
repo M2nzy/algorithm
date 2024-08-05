b946699a0044efff2780dace8189635c208f3e2c
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-08-05 14:33:08 +0900

    [level 2] Title: 주식가격, Time: 17.14 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,29 +1,25 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Deque<Integer> stack = new ArrayDeque<>();
+        int[] count = new int[prices.length];
+        int len = prices.length;
         
-        for (int i=0;i<prices.length;i++){
-            while(!stack.isEmpty()){
-                int j = stack.peek();
-                if (prices[j]> prices[i]){
-                    answer[j] = i-j;
-                    stack.pop();
+        for (int i=0; i<len; i++){
+            for(int j=i+1; j<len; j++){
+                if(prices[i] == prices[j]){
+                    count[i] += 1;
                 }
-                else{
+                else if(prices[i] < prices[j]){
+                    count[i] +=1;
+                    
+                }
+                else if(prices[i] > prices[j]){
+                    count[i] += 1;
                     break;
                 }
             }
-            stack.push(i);
         }
         
-        while(!stack.isEmpty()){
-            int i = stack.pop();
-            answer[i] = prices.length-i-1;
-        }
-        
-        
-        return answer;
-    }
+        return count;
+    }       
 }

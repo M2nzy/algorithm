ccf694f4c7b957ab11ebf9c75f45576b7bc25319
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-12-08 18:19:18 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 08일 18:13:38
+2024년 12월 08일 18:19:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,23 +2,32 @@ import itertools
 def solution(clothes):
     answer = 0
     zhong = dict()
-    
+    # 키 마다 빈 값의 딕셔너리 생성
     for i in range(len(clothes)):
         zhong[clothes[i][1]] = list()
+        
+    # 각 키에 리스트 아이템들 할당
     for i in range(len(clothes)):
         zhong[clothes[i][1]] += [clothes[i][0]]
-
+        
+    # 옷 종류 몇개인지 구하기
     a = len(zhong)
     result = 1
+    
+    # 옷 종류가 여러개일 경우
     if a > 1:
-        for i in zhong: # 종류에 따라 더하기
+        for i in zhong: 
+            # (그 종류 내용물에 따라 입을 수 있는 총 가지수 구하기(len) + 이 종류를 안 입는 경우 1개)
+            # => 종류별로 모두 곱하기
             result *= (len(zhong[i]) + 1)
-        answer = (result - 1)
+        answer = (result - 1) # 아예 모든 것을 아무것도 안 입는 경우 빼기
         return answer
-    elif a == 1: # 옷 종류가 1개뿐
+    
+    # 옷 종류가 1개일 경우
+    elif a == 1:
         for i in zhong: # 옷 하나씩 더하기
             result *= len(zhong[i])
         answer = result
         return answer
-    else:
+    else: # 무의미
         return 1

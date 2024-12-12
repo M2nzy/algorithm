e397086e5ceb9e5d47a9b8b0b1203ec371ba5271
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-12-13 00:49:45 +0900

    [level 2] Title: 귤 고르기, Time: 93.67 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,10 +1,6 @@
 from collections import Counter
 def solution(k, tangerine):
-    answer = 0
     tangerine.sort(reverse=True)
-    
-    heap = []
-    
     a = Counter(tangerine)
     result = []
     key = list(a.items())
@@ -12,17 +8,19 @@ def solution(k, tangerine):
 
     for i in range(len(key)):
         tmp = k - key[i][1]
+        # 딱 맞게 귤을 뺐을 경우
         if tmp == 0:
             result.append(key[i][0])
             k -= key[i][1]
             break
+        # [i][0]호에서 가지고 있는 귤이 더 많아서 빼다가 남을 경우
         elif tmp < k and tmp < 0:
             result.append(key[i][0])
             break    
+        # [i][0]호에 해당하는 귤은 다 뺐지만 아직 귤이 더 필요한 경우
         elif tmp <= k:
             result.append(key[i][0])
             k -= key[i][1]
-
         
         
     return len(result)

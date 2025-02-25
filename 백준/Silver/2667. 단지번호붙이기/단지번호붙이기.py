cd19b2318b1f14ed056b803d4ab8c9eb941f6743
M2nzy <36951721+M2nzy@users.noreply.github.com>
2025-02-26 01:37:56 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,46 +1,45 @@
-import sys
 from collections import deque
+import sys
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-input = sys.stdin.readline
-def bfs():
-    global q, visited, danji
-    cnt = 1
-    while(q):
+def bfs(n, maps, i, j):
+    q = deque()
+    q.append((i, j))
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    visited = [[False] * (n) for _ in range(n)]
+    count = 1
+    while q:
         x, y = q.popleft()
         visited[x][y] = True
+        maps[x][y] = 0
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if (visited[nx][ny] == False) and (danji[nx][ny] == '1'):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and maps[nx][ny] == 1:
                     q.append((nx, ny))
-                    danji[nx][ny] = 0
-                    cnt += 1
-    return cnt
+                    visited[nx][ny] = True
+                    maps[nx][ny] = 0
+                    count += 1
 
+    return count
 
-
-N = int(input())
-visited = [[False] * (N) for _ in range(N)]
-
-tmp = []
-
-danji = []
-for _ in range(N):
-    danji.append(list(input().strip()))
-
-q = deque()
+n = int(sys.stdin.readline().strip())
+global maps
+maps = []
+for i in range(n):
+    maps.append(list(map(int, sys.stdin.readline().strip())))
+                
 answer = []
+cnt = 0
+for i in range(n):
+    for j in range(n):
+        if maps[i][j] == 1:
+            r = bfs(n, maps, i, j)
+            cnt += 1
+            answer.append(r)
 
-for i in range(N):
-    for j in range(N):
-        if danji[i][j] == '1':
-            q.append((i,j))
-            answer.append(bfs())            
-
+print(cnt)
 answer.sort()
-print(len(answer))
-for i in range(len(answer)):
-    print(answer[i])
+for i in answer:
+    print(i)
